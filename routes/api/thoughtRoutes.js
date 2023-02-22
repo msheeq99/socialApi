22daa8eb5d15d890b13b5dddad9e28bc2ed5c64a
Mohamud <msheeq99@outlook.com>
2023-02-22 10:08:14 +0000

    thoughtRoutes and userRoutes js completed

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+
 const {
     getThought,
     getSingleThought,
@@ -11,4 +12,21 @@ const {
 
 // /api/thoughts
 router.route('/').get(getThought).post(createThought);
+
+// /api/thoughts/:thoughtId
+router.route('/:thoughtId')
+.get(getSingleThought)
+.put(updateThought)
+.delete(deleteThought);
+
+//  /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions')
+.post(createReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:reactionId')
+.delete(deleteReaction);
+
+
+module.exports = router;
 
