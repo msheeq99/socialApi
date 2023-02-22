b3cc9fa61cce30e72266d15c515cebe5d463a7ac
Mohamud <msheeq99@outlook.com>
2023-02-22 10:00:11 +0000

    thoughtRoutes.js import getThought and import createThought

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,1 +1,14 @@
 const router = require('express').Router();
+const {
+    getThought,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction
+} = require('../../controllers/thoughtController');
+
+// /api/thoughts
+router.route('/').get(getThought).post(createThought);
+
